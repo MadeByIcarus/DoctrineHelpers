9750fb707e87b4c20d7501626463941a4372c264
Pavel Gajdoš <info@pavelgajdos.cz>
2019-05-06 12:13:07 +0200

    Initial commit

---
+++ src/Attributes/BigIdentifier.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Inspired by Kdyby\Doctrine\Entities\Attributes\Identifier
+ */
+
+namespace Icarus\DoctrineHelpers\Entities\Attributes;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+trait BigIdentifier
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="bigint", options={"unsigned"=true})
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
+
+
+
+    /**
+     * @return integer
+     */
+    final public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+    public function __clone()
+    {
+        $this->id = NULL;
+    }
+
+}
