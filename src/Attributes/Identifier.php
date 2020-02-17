2200df6aeae4c104bdde27ee33a19c24a7960a77
Pavel Gajdoš <info@pavelgajdos.cz>
2020-02-17 11:41:13 +0100

    Add Identifier trait

---
+++ src/Attributes/Identifier.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Icarus\DoctrineHelpers\Entities\Attributes;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+trait Identifier
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
+
+
+
+    /**
+     * @return integer
+     */
+    final public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+    public function __clone()
+    {
+        $this->id = NULL;
+    }
+
+}
