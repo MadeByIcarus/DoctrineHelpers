5ab912cf814090ffe1ddeedef1d3c66356c9dfcf
Pavel Gajdoš <info@pavelgajdos.cz>
2020-02-17 12:16:36 +0100

    Add SignedIdentifier trait which replaces the deprecated Nettrine\ORM\Entity\Attributes\Id

---
+++ src/Attributes/SignedIdentifier.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Icarus\DoctrineHelpers\Entities\Attributes;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Trait SignedIdentifier
+ * @package Icarus\DoctrineHelpers\Entities\Attributes
+ *
+ * This replaces the deprecated trait Nettrine\ORM\Entity\Attributes\Id which was not unsigned and therefore the trait Identifier caused problems with migrations.
+ */
+trait SignedIdentifier
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
+
+
+
+    /**
+     * @return integer
+     */
+    final public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+    public function __clone()
+    {
+        $this->id = NULL;
+    }
+
+}
